b1f4a6868b8ce869a27a67da1f81c340df8c46df
Martin Iturbide <martiniturbide@gmail.com>
2026-05-18 21:07:43 -0500

    Update main.cpp
    Change made for OS/2 compile by Dave.

--- code/main.cpp
+++ code/main.cpp
@@ -29,7 +29,7 @@
     #include "life.h"
     #include "lifeController.h"
         extern "C" int
-        SDL_main( int argc, char** argv )
+        main( int argc, char** argv )
         {
                 unsigned int initFlags = 0;
                 initFlags |= SDL_INIT_VIDEO;
